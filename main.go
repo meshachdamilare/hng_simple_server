4e241a77542f5ae4b7b4375481465aea699e2177
meshachdamilare <meshacholuwadamilare@gmail.com>
2023-09-11 08:45:55 +0100

    add code file

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type student struct {
+	SlackName     string `json:"slack_name"`
+	CurrentDay    string `json:"current_day"`
+	UTCTime       string `json:"utc_time"`
+	Track         string `json:"track"`
+	GithubFileURL string `json:"github_file_url"`
+	GithubRepoURL string `json:"github_repo_url"`
+	StatusCode    int    `json:"status_code"`
+}
+
+func main() {
+	port := "3000"
+	r := gin.New()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "Server works")
+	})
+	r.GET("/api", doSomething)
+	r.Run(":" + port)
+}
+
+func doSomething(c *gin.Context) {
+	sName := strings.ToLower(c.Query("slack_name"))
+	track := strings.ToLower(c.Query("track"))
+
+	fmt.Println("sName", sName)
+	fmt.Println("track", track)
+
+	var st student
+	{
+		st.SlackName = "meshach"
+		st.Track = "backend"
+		st.GithubFileURL = ""
+		st.GithubRepoURL = ""
+		st.CurrentDay = time.Now().Weekday().String()
+		st.UTCTime = time.Now().UTC().Format(time.RFC3339)
+		st.StatusCode = http.StatusOK
+	}
+
+	fmt.Println(st)
+
+	if sName == st.SlackName && track == st.Track {
+		c.JSON(http.StatusOK, st)
+		return
+	}
+	st.StatusCode = http.StatusBadRequest
+	c.JSON(http.StatusBadRequest, gin.H{"error": "request not found", "st": st.StatusCode})
+}
